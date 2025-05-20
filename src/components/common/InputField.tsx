e233b0473636650103f98fd62436fb4fd47b2dbc
Minh Tuyen <bachminhtuyen2003@gmail.com>
2025-05-20 22:58:56 +0700

    refactor(component): Change input field attribute structure

--- src/components/common/InputField.tsx
+++ src/components/common/InputField.tsx
@@ -8,24 +8,27 @@ export default function InputField({
     required,
     type = 'text',
     className = '',
+    placeholder = '',
 }: {
-    label: string;
+    label?: string;
     name: string;
     value: string;
     onChange: (e: ChangeEvent<HTMLInputElement>) => void;
     required?: boolean;
     type?: string;
     className?: string;
+    placeholder?: string;
 }) {
     return (
         <div className={className}>
-            <label className="mb-2 block text-sm font-medium text-gray-700">{label}</label>
+            {label && <label className="mb-2 block text-sm font-medium text-gray-700">{label}</label>}
             <input
                 type={type}
                 name={name}
                 value={value}
                 onChange={onChange}
                 required={required}
+                placeholder={placeholder}
                 className="w-full rounded-lg border border-gray-300 p-2 focus:ring focus:ring-blue-400 focus:outline-none"
             />
         </div>
